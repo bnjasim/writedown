048d7405356fe9e56f8d57a0d9ac3b923fa1966e
Binu Jasim <bnjasim@gmail.com>
2016-12-16 23:08:27 +0530

    Markdown to html conversion - good. But need to get the icons working next

---
+++ writedown.js
@@ -0,0 +1,7 @@
+var disp = document.getElementById('text-disp-div');
+var tarea = document.getElementById('textarea-div');
+
+tarea.addEventListener('input', function (evt) {
+    disp.innerHTML = marked(tarea.innerText);
+});
+
