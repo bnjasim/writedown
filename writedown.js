c8f522c416ef17dc53813ef9c4b2ecc16bdffd7a
Binu Jasim <bnjasim@gmail.com>
2016-12-18 16:00:41 +0530

    Bold and Italic: when clicked, wrap the selected text inside ** ** or * *

--- writedown.js
+++ writedown.js
@@ -1,8 +1,54 @@
 var disp = document.getElementById('text-disp-div');
 var tarea = document.getElementById('textarea-div');
+var icon_group = document.getElementById('textarea-icon-group');
 
 tarea.addEventListener('input', function (evt) {
-    disp.innerHTML = marked(tarea.innerText);
-    renderMathInElement(disp);
+    disp.innerHTML = window.marked(tarea.innerText);
+    window.renderMathInElement(disp);
 });
+
+
+// Button click handlers
+icon_group.addEventListener('click', function(event) {
+    // we care about two targets. Either on the button or on the span inside button
+    // Otherwise we don't take any action
+    var target_node = event.target; 
+    if (target_node.tagName !== 'BUTTON') 
+        target_node = target_node.parentElement;
+    
+    if (target_node.id) { // insert markdown only for valid buttons with an id
+		// create a new text node to insert at the caret position
+		var node = document.createTextNode('');
+		// Get the selected text
+		tarea.focus();
+		var sel = window.getSelection();
+		var range = sel.getRangeAt(0);
+		var selected_text = range.toLocaleString();
+		if (!selected_text)
+			selected_text = 'Text';
+		// To replace the selected text, delete it first
+		range.deleteContents();
+		
+		switch(target_node.id) {
+			case 'bold':
+				node.textContent = '**'+ selected_text +'**';
+				break;
+			case 'italic':
+				node.textContent = '*'+ selected_text +'*';
+		}
+		
+		// Then add the new content
+		range.insertNode(node);
+		// Select the new content
+		var new_range = document.createRange();
+		//new_range.selectNodeContents(node); // selcts all including ** **
+		new_range.setStart(node, 1);
+		new_range.setEnd(node, selected_text.length+1);
+		sel.removeAllRanges();
+		sel.addRange(new_range);
+		
+	}
+})
+
+
 
